ff6c731e66a85a6fce537f4008a1504289ae1d6c
Liam Abourousse <75449798+Bardakor@users.noreply.github.com>
2021-03-22 19:40:52 +0100

    TP0

---
+++ tp0-liam-alexandre.abourousse/Basics/Basics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Basics
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                // This is the first function that will run in your program.
+                // You can put your tests here, e.g.:
+                //    Basics.HappyBirthday();
+                Basics.RotAlphaRight('t', 10);
+            }
+        }
+    }
+}
